5affdeff1bb7702651ed4247ccc4c42c38c58b51
tobbenda <tobbenda@gmail.com>
2020-03-20 18:51:11 +0100

    atombranch commit3

---
+++ Atomfile2.java
@@ -0,0 +1,1 @@
+atomfile 2
--- Test3.java
+++ Test3.java
@@ -3,5 +3,6 @@ class Under{
   public static void main(String[] args) {
     System.out.println("Under");
     System.out.println("ENDRING");
+    System.out.println("ENDRING Igjen");
   }
 }
